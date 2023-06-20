5513e15b8b6c068113aa159b7152629566857b09
smit_valture <valture16@gmail.com>
2023-06-20 23:51:11 +0530

    init game

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='w-screen h-screen flex justify-center items-center bg-[#d6e0ed]'>
+      <div className='w-full mx-4 md:w-80 p-4 pb-10 flex flex-col gap-6 justify-center items-center bg-white shadow-xl rounded-xl'></div>
+    </section>
+  )
+}
+
+export default Home
